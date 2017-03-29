ec1e71da67cfff896380c8a9629498fe893e65cf
Tyler Johnson <tyler@beneaththeink.com>
2017-03-29 16:28:16 -0600

    feat: export rules directly

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 import Blueprint from "./blueprint";
-import Schema, {defaultSchema} from "./schema";
+import Schema from "./schema";
 import Field from "./field";
-import * as rules from "./rules/index";
+import legacySection from "./rules/legacy-section";
+import legacyRoot from "./rules/legacy-root";
+import section from "./rules/section";
+import list from "./rules/list";
+import defaults from "./rules/defaults";
+
+const rules = [
+  legacySection,
+  legacyRoot,
+  section,
+  list,
+  defaults
+];
+
+const defaultSchema = Schema.create(rules);
+
+function createBlueprint(field, schema=defaultSchema) {
+  return Blueprint.create({ root: field, schema });
+}
 
 export {
+  createBlueprint as default,
   Blueprint,
   Schema,
   Field,
   defaultSchema,
-  rules
+  rules,
+  legacySection,
+  legacyRoot,
+  section,
+  list,
+  defaults
 };
-
-export default function(field, schema=defaultSchema) {
-  return Blueprint.create({ root: field, schema });
-}

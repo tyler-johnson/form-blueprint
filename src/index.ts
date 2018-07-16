610b185e45882987fcc34bd34ea24a38e42ceba9
Tyler Johnson <tyler@tylerjohnson.me>
2018-07-16 11:23:04 -0600

    fix: correct main file, export everything

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import { Blueprint } from "./blueprint";
 import { Schema, Rule, SchemaCreate } from "./schema";
 import { Field, FieldCreate } from "./field";
+
 import legacySection from "./rules/legacy-section";
 import legacyRoot from "./rules/legacy-root";
 import section from "./rules/section";
 import list from "./rules/list";
 import defaults from "./rules/defaults";
 
-const rules = [
+export * from "./blueprint";
+export * from "./schema";
+export * from "./field";
+
+export const rules = [
   legacySection,
   legacyRoot,
   section,
@@ -15,22 +20,16 @@ const rules = [
   defaults
 ];
 
-const defaultSchema = Schema.create(rules);
-
-function createBlueprint(root?: FieldCreate, schema: SchemaCreate = defaultSchema) {
-  return Blueprint.create({ root, schema });
-}
-
 export {
-  createBlueprint as default,
-  Blueprint,
-  Schema,
-  Field,
-  defaultSchema,
-  rules,
   legacySection,
   legacyRoot,
   section,
   list,
   defaults
 };
+
+export const defaultSchema = Schema.create(rules);
+
+export default function createBlueprint(root?: FieldCreate, schema: SchemaCreate = defaultSchema) {
+  return Blueprint.create({ root, schema });
+}
